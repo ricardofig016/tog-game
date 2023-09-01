1a15edbd082f0c045ef11adad6d6a21c199088dd
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-09-01 21:49:35 +0100

    changed grid_size display from static to fluid

--- floor_test.py
+++ floor_test.py
@@ -7,11 +7,15 @@ class FloorTest(Window):
         self.NAME = NAME
         self.FLOOR = FLOOR
         self.GRID_SIZE = GRID_SIZE
-        self.GRID_CELL_SIZE = 60
-        self.GRID_COLOR = (100, 100, 100)
+        self.GRID_COLOR = (50, 50, 50)
         self.TEAM_SIZE = TEAM_SIZE
         CAPTION = f"{self.FLOOR} - {self.NAME}"
         super().__init__(CAPTION=CAPTION, BG_IMAGE_PATH=BG_IMAGE_PATH)
+        self.calc_grid_cell_size()
+
+    def calc_grid_cell_size(self):
+        space = 3 / 5 * self.WIDTH
+        self.GRID_CELL_SIZE = int(space / (self.GRID_SIZE + 2))
 
     def get_info(self) -> str:
         info = self.name
@@ -23,32 +27,29 @@ class FloorTest(Window):
             self.GRID_COLOR,
             (self.WIDTH * (1 / 5), 0),
             (self.WIDTH * (1 / 5), self.HEIGHT),
-            5,
+            6,
         )
         pygame.draw.line(
             self.screen,
             self.GRID_COLOR,
             (self.WIDTH - self.WIDTH * (1 / 5), 0),
             (self.WIDTH - self.WIDTH * (1 / 5), self.HEIGHT),
-            5,
+            6,
         )
         self.draw_grid()
         return
 
     def draw_grid(self) -> None:
         start_x = int(1 / 5 * self.WIDTH) + self.GRID_CELL_SIZE
-        end_x = int(4 / 5 * self.WIDTH) - self.GRID_CELL_SIZE
+        end_x = start_x + self.GRID_CELL_SIZE * self.GRID_SIZE
         start_y = int((self.HEIGHT - 3 / 5 * self.WIDTH) / 2) + self.GRID_CELL_SIZE
-        end_y = (
-            int(self.HEIGHT - (self.HEIGHT - 3 / 5 * self.WIDTH) / 2)
-            - self.GRID_CELL_SIZE
-        )
+        end_y = start_y + self.GRID_CELL_SIZE * self.GRID_SIZE
 
         # vertical lines
         for x in range(start_x, end_x + 1, self.GRID_CELL_SIZE):
-            pygame.draw.line(self.screen, self.GRID_COLOR, (x, start_y), (x, end_y), 2)
+            pygame.draw.line(self.screen, self.GRID_COLOR, (x, start_y), (x, end_y), 4)
 
         # horizontal lines
         for y in range(start_y, end_y + 1, self.GRID_CELL_SIZE):
-            pygame.draw.line(self.screen, self.GRID_COLOR, (start_x, y), (end_x, y), 2)
+            pygame.draw.line(self.screen, self.GRID_COLOR, (start_x, y), (end_x, y), 4)
         return
