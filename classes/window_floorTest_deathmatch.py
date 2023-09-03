8396f53256e04d9c1a8286224bf1b068f2ef9f32
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-09-03 21:40:08 +0100

    method populate_grid

--- classes/window_floorTest_deathmatch.py
+++ classes/window_floorTest_deathmatch.py
@@ -1,18 +1,29 @@
 import random
 import os
 from classes.window_floorTest import FloorTest
+from classes.cell import Cell
 
 
 class Deathmatch(FloorTest):
     def __init__(self) -> None:
         super().__init__(
             name="Deathmatch Test",
-            floor="2F/Evankhell's hell",
-            grid_size=12,
-            team_a_size=1,
+            floor="2F",
+            grid_size=5,
+            team_a_size=2,
             team_b_size=1,
             bg_image_path=self.get_random_bg_image(),
+            prompt="Team up with Khun and defeat Rak",
         )
+        self.populate_grid()
+
+    def populate_grid(self):
+        self.grid[0][2].set_element("boulder")
+        self.grid[1][3].set_element("boulder")
+        self.grid[2][3].set_element("boulder")
+        self.insert_character(3, "a", 4, 0)
+        self.insert_character(4, "n", 1, 1)
+        self.insert_character(5, "b", 0, 4)
 
     def get_random_bg_image(self):
         dir_path = "assets/deathmatch_test"
