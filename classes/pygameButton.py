828201c9881154a88810cc357db86d6dc53961d3
ricardofig016 <ricardifigueiredo016@gmail.com>
2023-09-04 17:51:29 +0100

    added auto font manker

--- classes/pygameButton.py
+++ classes/pygameButton.py
@@ -1,9 +1,21 @@
+import pygame
+
+
 class PygameButton:
-    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+    def __init__(
+        self,
+        image,
+        pos,
+        text_input,
+        size,
+        base_color,
+        hovering_color,
+        font,
+    ):
         self.image = image
         self.x_pos = pos[0]
         self.y_pos = pos[1]
-        self.font = font
+        self.font = pygame.font.Font(font, int(size))
         self.base_color, self.hovering_color = base_color, hovering_color
         self.text_input = text_input
         self.text = self.font.render(self.text_input, True, self.base_color)
